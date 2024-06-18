bf95255e2a8021b6e95e57ff1143f4a5b1533152
Rahul <rahul.nuthalapati17@gmail.com>
2024-06-18 13:01:43 -0400

    Navbar changes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementById("content").textContent = ""
+let navbar = document.getElementById("navbar");
+window.onscroll = function() {
+    if (window.scrollY > navbar.offsetHeight) {
+        navbar.style.backgroundColor = "rgba(256, 256, 256, 0.95)";
+        navbar.style.position = "sticky";
+        navbar.style.top = "0";
+        navbar.querySelector("h1").style.fontWeight = "600";
+    }
+    else
+    {
+        navbar.style.backgroundColor = "rgba(256, 256, 256, 1)";
+        navbar.style.position = "";
+        navbar.style.top = "";
+        navbar.querySelector("h1").style.fontWeight = "";
+    }
+};
+
